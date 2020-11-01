0db4979985fa704bac9c9c687b0dad5a2b128dbf
WillsECEstuff <40055081+WillsECEstuff@users.noreply.github.com>
2020-11-01 13:51:38 -0800

    Fixed 2nd map not properly resetting + renames

---
+++ WarehouseApp/mainwhmap.h
@@ -0,0 +1,45 @@
+#ifndef mainwhmap_H
+#define mainwhmap_H
+
+#include <QMainWindow>
+#include <QPainter>
+#include <QVector>
+#include <QPointF>
+#include <QtWidgets>
+#include "secondproductwindow.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class mainwhmap; }
+QT_END_NAMESPACE
+
+class mainwhmap : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit mainwhmap(QWidget *parent = 0);
+    ~mainwhmap();
+
+    void loadAllPoints(QVector <QPointF> ptsList);
+    void loadProductPoints(QVector <QPointF> ptsList);
+    void loadRoutePrinter(QVector <QPointF> route);
+    void loadInstructions(QVector <std::string> instrList);
+
+    virtual void paintEvent(QPaintEvent *event);
+
+private slots:
+    void handleButton();
+
+private:
+    Ui::mainwhmap *ui;
+    QVector <QPointF> allPoints;
+    QVector <QPointF> productPoints;
+    QVector <QPointF> routePoints;
+    QVector <std::string> directions;
+    QPushButton *p_button;
+    QLineEdit *txtID;
+    QLabel *txtLbl;
+    secondProductWindow *secWindow;
+    QVector <QPointF> singleRoute;
+};
+#endif // mainwhmap_H
