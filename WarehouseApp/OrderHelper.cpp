8ec8d59d592f625c96b5a3cd78c04533b115c9b3
tonyn9 <tonyn9@uci.edu>
2020-11-05 21:13:45 -0800

    Added documentation to orderHelper

--- WarehouseApp/OrderHelper.cpp
+++ WarehouseApp/OrderHelper.cpp
@@ -1,15 +1,32 @@
-
+/*****************************************************************//**
+ * @file   OrderHelper.cpp
+ * @brief  This file is contains the definitions for OrderHelper.h
+ * 
+ * @author Tony
+ * @date   November 2020
+ *********************************************************************/
 
 
 #include "OrderHelper.h"
 
-
-Order createOrderfromVector(Database* d, std::vector<std::string> productList,
+/**
+ * @brief	Creates and returns an Order object. This functions takes
+ *			a vector of productIDs in string form, finds the location
+ *			in the database, and creates an order out of them. If 
+ *			a product does not exist within a database, it is ignored.
+ *			oID and priority are both default 1.
+ * 
+ * @param	productList		a vector of strings containing product IDs
+ * @param	oID				int, should be a non negative number
+ * @param	priority		int, should be integer from 1-100
+ * @return	Order 
+ */
+Order createOrderfromVector(std::vector<std::string> productList,
 	int oID, int priority)
 {
-	if (d == NULL) {
-		d = d->getInstance();
-	}
+	
+	Database* d = d->getInstance();
+	
 	Order o(oID, priority);
 	std::tuple<float, float> t;
 
@@ -27,12 +44,22 @@ Order createOrderfromVector(Database* d, std::vector<std::string> productList,
 	return o;
 }
 
-Order createOrderfromProductID(Database* d, std::string pID,
-	int oID, int priority)
+/**
+ * @brief	Creates and returns an Order object. This functions takes
+ *			a productID a in string form, finds the location
+ *			in the database, and creates an order out of it. If 
+ *			a product does not exist within a database, the function will
+ *			throw a logic_error so the information can be relayed by the
+ *			GUI.
+ * 
+ * @param	pID			string, productID in string form
+ * @param	oID			int, should be a non negative number
+ * @param	priority	int, should be integer from 1-100
+ * @return	Order
+ */
+Order createOrderfromProductID(std::string pID, int oID, int priority)
 {
-	if (d == NULL) {
-		d = d->getInstance();
-	}
+	Database* d = d->getInstance();
 	Order o(oID, priority);
 	std::tuple<float, float> t;
 
@@ -51,16 +78,23 @@ Order createOrderfromProductID(Database* d, std::string pID,
 	return o;
 }
 
-void addVectortoExistingOrder(Database* d, std::vector<std::string> productList,
+/**
+ * @brief	This function adds products to an existing order.
+ *			This function takes a vector of productIDs in string form,
+ *			finds the location in the database, and adds it to the 
+ *			order. ProductIDs not in the database are ignored.
+ * 
+ * @param	productList		a vector of strings containing product IDs
+ * @param	o				Order to be added
+ */
+void addVectortoExistingOrder(std::vector<std::string> productList,
 	Order* o) 
 {
 	
 	if (o == NULL) {
 		return;
 	}
-	if (d == NULL) {
-		d = d->getInstance();
-	}
+	Database* d = d->getInstance();
 
 	std::tuple<float, float> t;
 	for (std::vector<std::string>::iterator it = productList.begin(); it != productList.end(); ++it) {
@@ -76,15 +110,25 @@ void addVectortoExistingOrder(Database* d, std::vector<std::string> productList,
 
 }
 
-void addProducttoExistingOrder(Database* d, std::string pID, Order* o) 
+/**
+ * @brief	This function adds a single product to an existing order.
+ *			This function takes a productID in string form,
+ *			finds the location in the database, and adds it to the
+ *			order. ProductIDs not in the database are ignored. If 
+ *			a product does not exist within a database, the function will
+ *			throw a logic_error so the information can be relayed by the
+ *			GUI.
+ *
+ * @param	pID				string, productID in string form
+ * @param	o				Order to be added
+ */
+void addProducttoExistingOrder(std::string pID, Order* o) 
 {
 
 	if (o == NULL) {
 		return;
 	}
-	if (d == NULL) {
-		d = d->getInstance();
-	}
+	Database* d = d->getInstance();
 
 	std::tuple<float, float> t;
 
--- WarehouseApp/OrderHelper.h
+++ WarehouseApp/OrderHelper.h
@@ -1,3 +1,16 @@
+/*****************************************************************//**
+ * @file   OrderHelper.h
+ * @brief  This is the header file for helper functions regarding 
+ *			Orders. The main functionality is creating an order
+ *			based off a vector of strings or single string, or 
+ *			adding products to an order by a vector of strings
+ *			or a string.
+ *			
+ * 
+ * @author Tony
+ * @date   November 2020
+ *********************************************************************/
+
 #pragma once
 
 #include <vector>
@@ -9,8 +22,12 @@
 #include "Product.h"
 
 
-Order createOrderfromVector(Database* d, std::vector<std::string> productList,
+Order createOrderfromVector(std::vector<std::string> productList,
 	int oID = 1, int priority = 1);
 
-Order createOrderfromProductID(Database* d, std::string pID,
-	int oID = 1, int priority = 1);
+Order createOrderfromProductID(std::string pID, int oID = 1, int priority = 1);
+
+void addVectortoExistingOrder(std::vector<std::string> productList,
+	Order* o);
+
+void addProducttoExistingOrder(std::string pID, Order* o);
